af5293c388a471bdf76e22a9687537008ab90401
yshkd <yushi.public@gmail.com>
2020-09-25 00:25:09 +0900

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by yushi kida on 2020/09/25.
+//  Copyright © 2020 yushi kida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
