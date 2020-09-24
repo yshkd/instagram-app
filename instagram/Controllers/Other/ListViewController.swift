f54d6d06a8280f472ee46f97225e484794e4ff59
yshkd <yushi.public@gmail.com>
2020-09-25 01:20:12 +0900

    First commit

---
+++ instagram/Controllers/Other/ListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListViewController.swift
+//  instagram
+//
+//  Created by yushi kida on 2020/09/25.
+//  Copyright © 2020 yushi kida. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
